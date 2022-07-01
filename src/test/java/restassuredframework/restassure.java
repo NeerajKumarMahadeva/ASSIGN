9974d9b1c1ebd593e052319d0857c3d9a6c8cd2d
Neeraj kumar Mahadeva <neeraj.kumar.mahadeva@ibm.com>
2022-07-01 17:03:18 +0530

    first commit

---
+++ src/test/java/restassuredframework/restassure.java
@@ -0,0 +1,14 @@
+package restassuredframework;
+
+import org.testng.annotations.Test;
+
+//import org.junit.Test;
+
+public class restassure {
+	@Test
+	public void test(){
+		System.out.println("Hello World!");
+		
+		
+	}
+}
